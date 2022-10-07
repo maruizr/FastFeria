d284c4962a9533877b5ec3dfa7e72064bb156ab1
chrx3 <ciat.0313@gmail.com>
2022-10-07 17:45:28 -0300

    Listar y conexion oracle cloud

--- feria/urls.py
+++ feria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import index, login
+# from .views import index, login, listarVentLocal
 
-urlpatterns=[ 
-    path('', index, name="index"),
-    path('login/', login, name="login"),
-]
+# urlpatterns=[ 
+#     path('', index, name="index"),
+#     path('login', login, name="login"),
+#     path('ventas',listarVentLocal, name="ventas")
+# ]
--- feria/views.py
+++ feria/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+# from .models import VentLocal, VentExtran
 
-# Create your views here.
+# # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'registration/login.html')
+# def login(request):
+#     return render(request, 'registration/login.html')
+
+# #Formato del Listar
+# def listarVentLocal(request):
+#     #Accediendo al objeto que contiene los datos en la base de datos
+#     #con el .all() se traen todos los objetos que esten en la tabla
+#     ventas = VentLocal.objects.all()
+#     # con esta variable 'ventas' se le pasa los datos al template
+#     datos = {
+#         'ventas': ventas
+#     }
+
+#     #ahora hay que retornar todo para que lo envie al template
+#     return render(request, 'ventas/listarVentaLocal.html', datos)
