50e38f247f03f708c769d21f35e8e3864ce6a206
Maribel <maribelruizr303@gmail.com>
2022-10-20 02:58:03 -0300

    Se agregan procedimientos almacenados en el pa.sql

--- pa.sql
+++ pa.sql
@@ -21,14 +21,14 @@ CREATE OR REPLACE PROCEDURE FASTFERIA.SP_AGREGAR_PRODUCTO(
     v_precio_prod NUMBER,
     v_desc_prod VARCHAR2,
     v_stock_prod NUMBER,
-    v_usuarios_rut VARCHAR2,
+    v_usuarios_id NUMBER,
     v_blob BLOB,
     v_salida OUT NUMBER
 )
 IS 
 BEGIN
-    INSERT INTO productos(nom_prod, precio_prod, desc_prod, stock_prod, usuarios_rut, foto)
-    VALUES(v_nom_prod, v_precio_prod, v_desc_prod, v_stock_prod, v_usuarios_rut, v_blob);
+    INSERT INTO productos(nom_prod, precio_prod, desc_prod, stock_prod, usuarios_id, foto)
+    VALUES(v_nom_prod, v_precio_prod, v_desc_prod, v_stock_prod, v_usuarios_id, v_blob);
     COMMIT;
     v_salida:=1;
 
